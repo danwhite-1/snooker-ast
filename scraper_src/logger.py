92663b2c639394b417b69d6072a8d8910ef9a464
Daniel White <daniel.j.white999@gmail.com>
2023-01-21 17:06:40 +0000

    logger refactor, 2 lines b/w functions

--- scraper_src/logger.py
+++ scraper_src/logger.py
@@ -1,7 +1,9 @@
 from enum import Enum
 from datetime import datetime
 
+
 logOutput = ["logfile"] # Needs to be a list so can be modified in main
+
 
 class logLevel(str, Enum):
     INFO = "INFO"
@@ -9,10 +11,12 @@ class logLevel(str, Enum):
     ERR = "ERROR"
     DEBUG = "DEBUG"
 
+
 def initLogs():
     logfile = open("snookerast_logs.txt", "a")
     logfile.write(f"\n{'-' * 60}\n")
     logfile.close()
+
 
 def log(level, message):
     if logOutput[0] == "logfile":
@@ -21,6 +25,7 @@ def log(level, message):
         logfile.close()
     else:
         print(createLogMessage(level, message), end="") # newline is handled by createLogMessage
+
 
 def createLogMessage(level, message):
     return f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}][{level}] {message}\n"
